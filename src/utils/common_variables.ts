bf9951d973ba74b8552f325350efa40810e9de12
Igor <770190@bk.ru>
2025-02-11 14:04:51 +0700

    Added .env file

--- src/utils/common_variables.ts
+++ src/utils/common_variables.ts
@@ -1,3 +1,3 @@
-const backend_base_url = 'http://localhost:5000'
+const backend_base_url = process.env.REACT_APP_BACKEND_BASE_URL;
 export const api_url = `${backend_base_url}/api`
 export const img_url =  `${backend_base_url}/uploads/`
