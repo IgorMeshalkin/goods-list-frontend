6fd466f5a6eb60b2cb396eb32248c972906d495e
Igor <770190@bk.ru>
2025-02-11 15:56:52 +0700

    Renamed query_provider file to common naming style

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {QueryProvider} from 'api/QueryProvider';
+import {QueryProvider} from './api/query_provider';
 import {LangProvider} from "./context/language_context";
 import { BrowserRouter as Router } from 'react-router-dom';
 
--- src/utils/common_functions.ts
+++ src/utils/common_functions.ts
@@ -1,5 +1,5 @@
 export const formatPrice = (num: number): string => {
-    const [left, right = "00"] = num.toFixed(2).split("."); // Округляем до двух знаков и разделяем
-    const formattedLeft = left.replace(/\B(?=(\d{3})+(?!\d))/g, " "); // Добавляем пробелы в левой части
+    const [left, right = "00"] = num.toFixed(2).split(".");
+    const formattedLeft = left.replace(/\B(?=(\d{3})+(?!\d))/g, " ");
     return `${formattedLeft}.${right}`;
 }
